1d59e56b9d95f901bdc3a5ad7ba9b45c035e22a3
Ashutosh <ashutoshpatel@yahoo.in>
2024-03-22 10:01:37 +0530

    fix: removed unused imports

--- src/main/java/info/ashutosh/configure/security/SecurityConfiguration.java
+++ src/main/java/info/ashutosh/configure/security/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package info.ashutosh.configure.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
